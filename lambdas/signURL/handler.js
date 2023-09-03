052c04c25ff920481e24cc58c88d6643984e0ec1
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-09-03 18:30:31 -0500

    refactor: Create a new folder for lambdas and fix get signed url

---
+++ lambdas/signURL/handler.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({ signatureVersion: 'v4' });
+
+module.exports.handler = async (event) => {
+  const filename = event.queryStringParameters.filename;
+  const signedURL = await s3.getSignedUrlPromise('putObject', {
+    Key: `upload/${filename}`,
+    Bucket: process.env.BUCKET_NAME,
+    Expires: 300,
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(signedURL)
+  };
+};
