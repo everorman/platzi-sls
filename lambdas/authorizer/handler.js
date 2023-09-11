8307745240a07a92148dd1fec52abb903fe4d11d
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-09-11 13:34:01 -0500

    refactor: Adding authorizer to get user

--- lambdas/authorizer/handler.js
+++ lambdas/authorizer/handler.js
@@ -1,21 +1,36 @@
-module.exports.handler = (event, context) => {
+module.exports.handler = (event, context, callback) => {
   const date = new Date();
   const minutes = date.getMinutes();
   const hour = date.getHours();
+  console.log(event.authorizationToken, '+++++', `Bearer ${process.env.SECRET_KEY}-${hour}-${minutes}`);
   if (event.authorizationToken === `Bearer ${process.env.SECRET_KEY}-${hour}-${minutes}`) {
-    return {
-      principalId: 'anonymous',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: event.methodArn
-          }
-        ]
-      }
-    };
+    console.log('Token OK', JSON.stringify(generateAllow('anonymous', event.methodArn)));
+    callback(null, generateAllow('anonymous', event.methodArn));
+  } else {
+    callback("Unauthorized");
   }
-  throw Error('Unauthorizer');
-}; 
+
+};
+
+// Help function to generate an IAM policy
+var generatePolicy = function (principalId, effect, resource) {
+  var authResponse = {};
+
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    var policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+var generateAllow = function (principalId, resource) {
+  return generatePolicy(principalId, 'Allow', resource);
+};
